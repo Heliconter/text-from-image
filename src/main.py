65c604943f2b0eac410a192ffb0c77237463241b
AlexRyzhickov <makseljoinb@gmail.com>
2021-04-22 11:32:32 +0300

    remove rust project, add python project (PyQt5)

---
+++ src/main.py
@@ -0,0 +1,82 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QFileDialog, QGraphicsScene
+
+from PIL import Image
+import pytesseract as tess
+
+# tess.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1150, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(5, 5, 810, 550))
+        self.graphicsView.setObjectName("graphicsView")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(820, 5, 322, 500))
+        self.listWidget.setObjectName("listWidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(820, 509, 322, 45))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(830, 10, 301, 491))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setText("")
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1150, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuMenu = QtWidgets.QMenu(self.menubar)
+        self.menuMenu.setObjectName("menuMenu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionAdd_image = QtWidgets.QAction(MainWindow)
+        self.actionAdd_image.setObjectName("actionAdd_image")
+        self.actionAdd_image.triggered.connect(lambda: self.openFile())
+        self.menuMenu.addAction(self.actionAdd_image)
+        self.menubar.addAction(self.menuMenu.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Load data"))
+        self.menuMenu.setTitle(_translate("MainWindow", "Menu"))
+        self.actionAdd_image.setText(_translate("MainWindow", "Add image"))
+
+
+    def openFile(self):
+        path = QFileDialog.getOpenFileName()[0]
+
+        pixmap = QPixmap(path)
+        scene = QGraphicsScene(0,0,pixmap.width(),pixmap.height())
+        scene.addPixmap(pixmap)
+        self.graphicsView.setScene(scene)
+
+        img = Image.open(path)
+        text = tess.image_to_string(img, lang='eng+rus')
+        self.label.setText(text)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
