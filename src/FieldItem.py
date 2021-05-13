dbe543c2ec6f582c54bc6a92c59ef53120690038
Heliconter <25860895+Heliconter@users.noreply.github.com>
2021-05-13 12:28:55 +0300

    Adding text input inside rect

--- src/FieldItem.py
+++ src/FieldItem.py
@@ -1,16 +1,17 @@
 from PySide2.QtWidgets import QGraphicsTextItem, QGraphicsRectItem, QGraphicsItemGroup, QGraphicsItem, QGraphicsPixmapItem
 from PySide2.QtCore import Qt, QRectF
-from PySide2.QtGui import QBrush, QPainter, QColor, QPen
+from PySide2.QtGui import QBrush, QPainter, QColor, QPen, QTextDocument
 
 class FieldItem(QGraphicsRectItem):
     def __init__(self, rect: QRectF, parent=None):
         super().__init__(parent)
 
-        self.setRect(rect)
-        self._rect = rect
+        self.setPos(rect.topLeft())
+
+        self.setRect(QRectF(0, 0, rect.width(), rect.height()))
 
         text = QGraphicsTextItem(self)
-        text.setParentItem(self)
+        text.document().setPageSize(self.rect().size())
         text.setPlainText('lsddjfk')
         text.setTextInteractionFlags(Qt.TextEditorInteraction)
 
@@ -18,4 +19,4 @@ class FieldItem(QGraphicsRectItem):
         painter.setPen(QPen(Qt.black))
         super().paint(painter, option, widget)
         painter.setBrush(QBrush(QColor(255, 255, 255, 128)))
-        painter.drawRect(self.boundingRect())
+        painter.drawRect(self.rect())
